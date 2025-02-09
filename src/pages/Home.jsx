81ed2289af75242b0ab194221a7b5b3dfed5c067
Azhar457 <azharsss457@gmail.com>
2025-02-09 18:43:00 +0700

    Upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -77,48 +77,10 @@ export default function Home() {
             ))}
           </div>
           <a
-            href="#section3"
+            href="Contact"
             className="mb-2 px-4 py-2 bg-red-800 text-white font-bold rounded-full text-sm text-center font-poppins"
           >
             Sejarah Perkembangan Islam
-          </a>
-        </div>
-      </section>
-      <section id="section3">
-        <div className="h-auto w-full bg-red-800 flex flex-col items-center justify-center text-white">
-          <h1 className="three text-3xl md:text-4xl font-bold font-poppins text-red-800 tracking-wide drop-shadow-md text-center">
-            SEJARAH <br />
-            PERKEMBANGAN ISLAM
-          </h1>
-          <div className="w-4/5 flex flex-col items-center justify-center place-self-center">
-            <p className="text-center text-white">
-              Islam adalah agama yang
-              <span className="text-yellow-400"> sempurna</span> dan
-              <span className="text-yellow-400"> universal</span> yang diturunkan
-              oleh Allah SWT kepada Nabi Muhammad SAW sebagai penutup para
-              Nabi. Islam adalah agama yang mengatur seluruh aspek kehidupan
-              manusia, mulai dari hubungan manusia dengan Allah SWT, hubungan
-              manusia dengan sesama manusia, hingga hubungan manusia dengan
-              alam sekitar.
-            </p>
-            <p className="text-center text-white">
-              Islam memiliki sejarah yang panjang, dimulai dari zaman Rasulullah
-              SAW hingga sekarang. Sejarah perkembangan Islam ini memiliki
-              berbagai peristiwa penting yang terjadi di berbagai belahan dunia.
-              Peristiwa-peristiwa tersebut membentuk Islam menjadi agama yang
-              besar dan kuat seperti yang kita kenal sekarang.
-            </p>
-            <p className="text-center text-white">
-              Dalam website ini, kami akan membahas sejarah perkembangan Islam
-              dari zaman Rasulullah SAW hingga sekarang. Kami akan membahas
-              berbagai peristiwa penting yang terjadi dalam sejarah Islam.
-            </p>
-          </div>
-          <a
-            href="#section2"
-            className="mt-2 px-4 py-2 bg-white text-red-800 font-bold rounded-full text-lg font-poppins"
-          >
-            Kembali ke atas
           </a>
         </div>
       </section>
