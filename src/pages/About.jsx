9bfc71203d1c2892f56589cb2cc07dade962e079
Azhar457 <azharsss457@gmail.com>
2025-02-23 20:05:41 +0700

    Update Link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -239,11 +239,17 @@ export default function Home() {
           <div class="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
               <div class="mt-12 md:mt-0">
-                <img
-                  src="https://images.unsplash.com/photo-1531973576160-7125cd663d86"
-                  alt="About Us Image"
-                  class="object-cover rounded-lg shadow-md"
-                />
+                <iframe
+                  className="object-cover rounded-lg shadow-md h-[25em] w-full"
+                  width="670"
+                  height="377"
+                  src="https://www.youtube.com/embed/dqVNIVlQqJE?start=1245"
+                  title="SEJARAH KHILAFAH dari awal hingga akhir"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
               </div>
               <div class="max-w-lg">
                 <h2 class="text-3xl font-extrabold text-left text-gray-900 sm:text-3xl">
@@ -317,11 +323,17 @@ export default function Home() {
                 </div>
               </div>
               <div class="mt-12 md:mt-0">
-                <img
-                  src="https://images.unsplash.com/photo-1531973576160-7125cd663d86"
-                  alt="About Us Image"
-                  class="object-cover rounded-lg shadow-md"
-                />
+                <iframe
+                  className="object-cover rounded-lg shadow-md h-[25em] w-full"
+                  width="670"
+                  height="377"
+                  src="https://www.youtube.com/embed/dqVNIVlQqJE?start=1380"
+                  title="SEJARAH KHILAFAH dari awal hingga akhir"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
               </div>
             </div>
           </div>
@@ -332,11 +344,17 @@ export default function Home() {
           <div class="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
               <div class="mt-12 md:mt-0">
-                <img
-                  src="https://images.unsplash.com/photo-1531973576160-7125cd663d86"
-                  alt="About Us Image"
-                  class="object-cover rounded-lg shadow-md"
-                />
+                <iframe
+                  className="object-cover rounded-lg shadow-md h-[25em] w-full"
+                  width="670"
+                  height="377"
+                  src="https://www.youtube.com/embed/dqVNIVlQqJE?start=1380"
+                  title="SEJARAH KHILAFAH dari awal hingga akhir"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
               </div>
               <div class="max-w-lg">
                 <h2 class="text-3xl font-extrabold text-left text-gray-900 sm:text-3xl">
